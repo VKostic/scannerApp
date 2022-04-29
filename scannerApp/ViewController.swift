aee508e9f9ac9c756c3fe789665ae2f3421a0d97
Vladica Kostic <v.kostic10@gmail.com>
2022-04-29 18:42:47 +0200

    Initial Commit

---
+++ scannerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scannerApp
+//
+//  Created by Vladica Kostic on 29.4.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
