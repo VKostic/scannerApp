bcf5baa8323897f3d0b8e950dde59207a7539760
Vladica Kostic <v.kostic10@gmail.com>
2022-06-14 10:52:29 +0200

    button za poziv

--- scannerApp/ViewController.swift
+++ scannerApp/ViewController.swift
@@ -6,15 +6,31 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var startButton: UIButton!
     
+    let Your_token = "fd81ba38-7e7c-49ba-8770-89321e2e0e04"
+    let Your_api = "jksdxjjfsdkhxyffiudfvhxnndueewpefodkcm439tufjifoas9"
+    let user_eMail = "test1@email.com"
+    let user_IMEI = "imei1"
+    let eMail = "test1@email.com"
+    
     let buttonAttributes: [NSAttributedString.Key: Any] = [
           .font: UIFont.systemFont(ofSize: 18),
           .underlineStyle: NSUnderlineStyle.single.rawValue
       ]
+    
+    @IBAction func sendTapped(_ sender: UIButton) {
+        let url = "https://mojeuredproxy-test.moj-eracun.hr/api/BI/Subject_SubjectHealthConfiguration/\(user_eMail)/\(user_IMEI)"
+        let headers: HTTPHeaders = [ "ApiKey" : "\(Your_api)"]
+        
+        AF.request(url, method: .get, headers: headers).responseString { response in
+            print(response.result)
+        }
+    }
     
     @IBAction func startScannerTapped(_ sender: UIButton) {
         guard let scannerView = self.storyboard?.instantiateViewController(withIdentifier: "ScannerVC") else { return }
@@ -27,6 +43,7 @@ class ViewController: UIViewController {
         startView.modalPresentationStyle = .fullScreen
         self.present(startView, animated: true, completion: nil)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let buttonText = NSMutableAttributedString(string: "Pokreni skener", attributes: buttonAttributes)
